c540bf1cc790847ca9cc6c1ded5d7a40b6888207
Cyber-dev27 <devashishahuja100@gmail.com>
2025-09-16 05:13:38 +0530

    basic structure

---
+++ script.js
@@ -0,0 +1,15 @@
+const techQuotes = [
+    "The best way to predict the future is to invent it. - Alan Kay",
+    "First, solve the problem. Then, write the code. - John Johnson",
+    "Technology is best when it brings people together. - Matt Mullenweg",
+    "Any sufficiently advanced technology is indistinguishable from magic. - Arthur C. Clarke",
+    "It's not a bug - it's an undocumented feature. - Anonymous",
+    "Move fast and break things. Unless you are breaking stuff, you are not moving fast enough. - Mark Zuckerberg",
+    "Innovation distinguishes between a leader and a follower. - Steve Jobs",
+    "Programs must be written for people to read, and only incidentally for machines to execute. - Harold Abelson",
+    "The computer was born to solve problems that did not exist before. - Bill Gates",
+    "Technology empowers us to do more, to learn more, and to be more. - Unknown"
+  ];
+
+  
+  
